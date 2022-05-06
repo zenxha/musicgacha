a78bacdb144d1a4c6512df61095b2e13513dad48
zenxha <komays5042@gmail.com>
2022-05-06 08:17:28 -0700

    roll and json

---
+++ src/main/java/com/musicgacha/controllers/RollController.java
@@ -0,0 +1,45 @@
+package com.musicgacha.controllers;
+import com.musicgacha.data.Song;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import com.musicgacha.data.Song;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+@Controller
+public class RollController {
+    @GetMapping("/roll")
+    public String song(Model model) throws IOException, ParseException {
+        //String web_server = "http://localhost:8080/";
+
+        ArrayList<Song> lis = new ArrayList<>();
+
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("src/main/resources/static/json/songs.json"));
+
+        JSONObject jsonObject = (JSONObject) obj;
+
+        for(Iterator iterator = jsonObject.keySet().iterator(); iterator.hasNext();) {
+            String key = (String) iterator.next();
+
+            // Song song = new Song();
+            JSONObject SongJSON = (JSONObject) jsonObject.get(key);
+            lis.add(new Song((String)SongJSON.get("trackTitle"), (String)SongJSON.get("artist"), (String)SongJSON.get("lyrics"), (String)SongJSON.get("spotify"), (String)SongJSON.get("youtube")));
+
+        }
+
+        // Printing song data in console
+        Song displaySong = lis.get((int) Math.floor(Math.random() * lis.size()));
+        System.out.println(displaySong.getTrackName() +" By "+ displaySong.getArtist() +"\nSpotify Link: "+ displaySong.getSpotifyUrl() + "\nYT Link: "+ displaySong.getYoutubeUrl()+"\n\n");
+        model.addAttribute("song", displaySong );
+        return "homesite/song";
+
+    }
+}
