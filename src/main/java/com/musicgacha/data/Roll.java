6861a97044b9e9b97e78fa6d99eb4400def6e2da
zenxha <komays5042@gmail.com>
2022-05-11 09:27:31 -0700

    Create class for characters

--- src/main/java/com/musicgacha/data/Roll.java
+++ src/main/java/com/musicgacha/data/Roll.java
@@ -17,25 +17,17 @@ public class Roll {
     private String origin;
     private String description;
     private String rarity;
-    public Roll(String rarity) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader("src/main/resources/static/json/characters/"+rarity+".json"));
-
-        JSONObject jsonObject = (JSONObject) obj;
-        JSONArray arr = (JSONArray) jsonObject.get("array");
-        JSONObject Roll = (JSONObject)arr.get((int) Math.floor(Math.random() * arr.size()));
+    public Roll(JSONObject r, String rarity)  {
         this.rarity = rarity;
-        this.image = (String)Roll.get("image");
-        this.name = (String)Roll.get("name");
-        this.origin = (String)Roll.get("origin");
-        this.description = (String)Roll.get("description");
+        image = (String)r.get("image");
+        name = (String)r.get("name");
+        origin = (String)r.get("origin");
+        description = (String)r.get("description");
     }
     public String getImage() {
         return image;
     }
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
     public String getDescription() {
         return description;
     }
