106baa55724a1810a9c978f32a79497b086b3c3d
zenxha <komays5042@gmail.com>
2022-05-12 12:46:55 -0700

    implement music gacha into index page

--- src/main/java/com/musicgacha/controllers/MainController.java
+++ src/main/java/com/musicgacha/controllers/MainController.java
@@ -3,17 +3,47 @@ package com.musicgacha.controllers;
  * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
  */
 
+import com.musicgacha.data.Song;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class MainController {
     @GetMapping("/")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) throws IOException, ParseException {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        ArrayList<Song> lis = new ArrayList<>();
+
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("src/main/resources/static/json/songs.json"));
+
+        JSONObject jsonObject = (JSONObject) obj;
+
+        for(Iterator iterator = jsonObject.keySet().iterator(); iterator.hasNext();) {
+            String key = (String) iterator.next();
+
+            // Song song = new Song();
+            JSONObject SongJSON = (JSONObject) jsonObject.get(key);
+            lis.add(new Song((String)SongJSON.get("trackTitle"), (String)SongJSON.get("artist"), (String)SongJSON.get("lyrics"), (String)SongJSON.get("spotify"), (String)SongJSON.get("youtube")));
+
+        }
+
+        // Printing song data in console
+        Song displaySong = lis.get((int) Math.floor(Math.random() * lis.size()));
+        model.addAttribute("song", displaySong );
         model.addAttribute("name", name); // MODEL is passed to html
         return "homesite/index"; // returns HTML VIEW (greeting)
 
