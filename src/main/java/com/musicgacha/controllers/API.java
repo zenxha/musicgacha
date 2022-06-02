9cee283c8bb2b157b3ccedc188c391d1f2331ec2
zenxha <komays5042@gmail.com>
2022-06-02 08:58:56 -0700

    get random character API

--- src/main/java/com/musicgacha/controllers/API.java
+++ src/main/java/com/musicgacha/controllers/API.java
@@ -1,49 +1,31 @@
 package com.musicgacha.controllers;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.musicgacha.data.Chae;
+import com.musicgacha.data.Roll;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import com.musicgacha.data.Song;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
 
 @RestController
 public class API {
-    @Autowired
 //    private ScoreSqlRepository repository;
-    @GetMapping("/api/random")
-    public ResponseEntity<Song> song() throws IOException, ParseException {
-        //String web_server = "http://localhost:8080/";
+    @Autowired
+    Chae chae = new Chae();
 
-        ArrayList<Song> lis = new ArrayList<>();
-
-
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader("src/main/resources/static/json/songs.json"));
-
-        JSONObject jsonObject = (JSONObject) obj;
-
-        for(Iterator iterator = jsonObject.keySet().iterator(); iterator.hasNext();) {
-            String key = (String) iterator.next();
-
-            // Song song = new Song();
-            JSONObject SongJSON = (JSONObject) jsonObject.get(key);
-            lis.add(new Song((String)SongJSON.get("trackTitle"), (String)SongJSON.get("artist"), (String)SongJSON.get("lyrics"), (String)SongJSON.get("spotify"), (String)SongJSON.get("youtube")));
-
-        }
-
-        // Printing song data in console
-        Song displaySong = lis.get((int) Math.floor(Math.random() * lis.size()));
-        System.out.println(displaySong.getTrackName() +" By "+ displaySong.getArtist() +"\nSpotify Link: "+ displaySong.getSpotifyUrl() + "\nYT Link: "+ displaySong.getYoutubeUrl()+"\n\n");
-        return new ResponseEntity<Song>(displaySong, HttpStatus.OK);
-
+    public API() throws Exception {
     }
+
+    @GetMapping("/api/random?rarity={rarity}")
+    public ResponseEntity<Roll> song(@PathVariable String rarity) throws IOException, ParseException {
+        //String web_server = "http://localhost:8080/";
+        return new ResponseEntity<Roll>(chae.getRandom(rarity), HttpStatus.OK);
+    }
+
+
+
 //    @PostMapping("/api/pushscore")
 //    public ResponseEntity<Score> saveScore(
 //            @RequestParam(value = "username", required = false) String username,
